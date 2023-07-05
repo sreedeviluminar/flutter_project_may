c070cc29241d510d059f1ff3e1d11100b45c07ad
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-07-05 12:10:41 +0530

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(MaterialApp(home: HomePage()));
+}
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("MyHome")),
       body: ListView(
         children: [
-          Text("Fruits"),
-          Image.asset(name)
+          const Center(child: Text("Fruits")),
+          Image.asset("assets/icons/apple.png"),
+          Image.asset("assets/icons/mango.png"),
+          Image.asset("assets/icons/orange.png"),
+          Image.asset("assets/icons/grapes.png"),
+          Image.asset("assets/icons/bananas.png"),
+          Image.asset("assets/icons/pineapple.png"),
         ],
       ),
     );
