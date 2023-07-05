1b3e9e95522f5597db04b4844229f106d75fad5e
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-07-05 12:59:47 +0530

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(home: HomePage()));
 }
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,9 +12,29 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: Text("MyHome")),
       body: ListView(
         children: [
-          const Center(child: Text("Fruits")),
-          Image.asset("assets/icons/apple.png"),
-          Image.asset("assets/icons/mango.png"),
+          Center(
+              child: Text("Fruits",
+                  style: GoogleFonts.dancingScript(
+                      fontSize: 80, color: Colors.green))),
+          Card(
+            child: ListTile(
+              leading: Image.asset("assets/icons/apple.png"),
+              title: Text("Apple"),
+              subtitle: Text("\$200"),
+              trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart)),
+            ),
+          ),
+          const Card(
+            color: Colors.lightGreen,
+            elevation: 5,
+            shadowColor:Colors.red,
+            child: ListTile(
+                leading: CircleAvatar(backgroundImage: AssetImage("assets/images/greennew.jpg")),
+                title: Text("Mango"),
+                subtitle: Text("\$100"),
+                trailing: Icon(Icons.shopping_cart),),
+          ),
+          
           Image.asset("assets/icons/orange.png"),
           Image.asset("assets/icons/grapes.png"),
           Image.asset("assets/icons/bananas.png"),
@@ -22,4 +44,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
