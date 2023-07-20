650c1a95f7236c8d5df28ca08d3e32ac82f4fc39
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-07-20 12:38:06 +0530

    commit

--- lib/carousal slider ex.dart
+++ lib/carousal slider ex.dart
@@ -1,63 +1,75 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(home: CSlider(),));
+}
 
 class CSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CarouselSlider(items: [
-        Container(
-            decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  "https://images.unsplash.com/photo-1689249876759-10ff4bc9f189?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=392&q=80")),
-        )),
-        Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      "https://images.unsplash.com/photo-1689781922550-81cacef1d933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80")),
-            )),
-        Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      "https://images.unsplash.com/photo-1689781922550-81cacef1d933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80")),
-            )),
-        Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      "https://images.unsplash.com/photo-1689249876759-10ff4bc9f189?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=392&q=80")),
-            )),
-        Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      "https://images.unsplash.com/photo-1689781922550-81cacef1d933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80")),
-            )),
-        Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      "https://images.unsplash.com/photo-1689781922550-81cacef1d933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80")),
-            )),
-      ], options: CarouselOptions(
-        height: 400,
-        aspectRatio: 16/9,
-        viewportFraction: .6,
-        initialPage: 0,
-        enableInfiniteScroll: true,
-        autoPlay: true,
-        autoPlayInterval: Duration(seconds: 2),
-        autoPlayCurve: Curves.elasticIn,
-        enlargeCenterPage: true,
-        enlargeFactor: .3,
-        scrollDirection: Axis.horizontal
-
-      )),
-    );
+        body: CarouselSlider(
+            items: List.generate(10, (index) =>
+                const Card(
+                  color: Colors.green,
+                  child: Center(
+                      child: Icon(
+                        Icons.ac_unit,
+                        size: 40,)),
+                )),
+            //[
+            //   Container(
+            //       decoration: const BoxDecoration(
+            //       image: DecorationImage(
+            //         fit: BoxFit.cover,
+            //         image: NetworkImage(
+            //             "https://images.unsplash.com/photo-1689249876759-10ff4bc9f189?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=392&q=80")),
+            //   )),
+            //   Container(
+            //       decoration: const BoxDecoration(
+            //         image: DecorationImage(
+            //             image: NetworkImage(
+            //                 "https://images.unsplash.com/photo-1689781922550-81cacef1d933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80")),
+            //       )),
+            //   Container(
+            //       decoration: const BoxDecoration(
+            //         image: DecorationImage(
+            //             image: NetworkImage(
+            //                 "https://images.unsplash.com/photo-1689781922550-81cacef1d933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80")),
+            //       )),
+            //   Container(
+            //       decoration: const BoxDecoration(
+            //         image: DecorationImage(
+            //             image: NetworkImage(
+            //                 "https://images.unsplash.com/photo-1689249876759-10ff4bc9f189?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=392&q=80")),
+            //       )),
+            //   Container(
+            //       decoration: const BoxDecoration(
+            //         image: DecorationImage(
+            //             image: NetworkImage(
+            //                 "https://images.unsplash.com/photo-1689781922550-81cacef1d933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80")),
+            //       )),
+            //   Container(
+            //       decoration: const BoxDecoration(
+            //         image: DecorationImage(
+            //             image: NetworkImage(
+            //                 "https://images.unsplash.com/photo-1689781922550-81cacef1d933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80")),
+            //       )),
+            // ],
+            options: CarouselOptions(
+            height: 800,
+            aspectRatio: 16 / 9,
+            viewportFraction: .5,
+            initialPage: 2,
+            enableInfiniteScroll: true,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 2),
+            autoPlayCurve: Curves.easeInOutCubicEmphasized,
+            enlargeCenterPage: false,
+            enlargeFactor: .3,
+            scrollDirection: Axis.vertical
+        )),);
   }
 }
