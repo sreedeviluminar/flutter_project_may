247b605d4d07bb14025d3c6f2399cd3827746c71
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-07-27 12:18:47 +0530

    commit

---
+++ lib/onboardingScreen Ex.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnBoardingEx extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    PageDecoration pageDecoration = const PageDecoration(
+        titleTextStyle: TextStyle(
+            color: Colors.purple,
+            fontStyle: FontStyle.italic,
+            fontSize: 30,
+            fontWeight: FontWeight.bold),
+        bodyTextStyle: TextStyle(fontSize: 18, color: Colors.pink),
+        imagePadding: EdgeInsets.all(20),
+        boxDecoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Colors.orange,
+          Colors.deepOrange,
+          Colors.red,
+          Colors.redAccent
+        ], begin: Alignment.topRight, end: Alignment.bottomLeft)));
+
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          decoration: pageDecoration,
+          image: Align(
+            // to place the widget at a desired position
+            alignment: Alignment.topCenter,
+            child: Image.asset("assets/images/logingirl.png"),
+          ),
+          title: "First Page",
+          body:
+              "With Microsoft 365 for families, you can create, organize, and collaborate, all in one place",
+        ),
+        PageViewModel(
+            image: Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset("assets/images/logingirl.png"),
+            ),
+            title: "SecondPage",
+            body:
+                "With Microsoft 365 for families, you can create, organize, and collaborate, all in one place"),
+        PageViewModel(
+            image: Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset("assets/images/logingirl.png"),
+            ),
+            title: "Third Page",
+            body:
+                "With Microsoft 365 for families, you can create, organize, and collaborate, all in one place")
+      ],
+    );
+  }
+}
