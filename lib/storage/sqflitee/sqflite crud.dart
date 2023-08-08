48e1676ebf3f65031c492ff009d6552dec073b2a
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-08 12:51:00 +0530

    commit

--- lib/storage/sqflitee/sqflite crud.dart
+++ lib/storage/sqflitee/sqflite crud.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'dboperation.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: SqfliteCrud(),
+  ));
+}
+
 class SqfliteCrud extends StatefulWidget {
   @override
   State<SqfliteCrud> createState() => _SqfliteCrudState();
@@ -10,22 +16,24 @@ class SqfliteCrud extends StatefulWidget {
 class _SqfliteCrudState extends State<SqfliteCrud> {
   final name_controller = TextEditingController();
   final phone_controller = TextEditingController();
-  var isloading = true;     // intially ui is in loading state
-  List<Map<String,dynamic>> contacts =[];  // empty list for storing data from db
+
+  var isloading = true; // intially ui is in loading state
+  List<Map<String, dynamic>> contacts =
+      []; // empty list for storing data from db
 
   @override
   void initState() {
     loadData();
     super.initState();
   }
-  void loadData() async{
+
+  void loadData() async {
     //read data from db
     final data = await SQLHelper.readData();
     setState(() {
       contacts = data;
-      isloading= false;
+      isloading = false;
     });
-
   }
 
   @override
@@ -37,13 +45,42 @@ class _SqfliteCrudState extends State<SqfliteCrud> {
       floatingActionButton:
           //here null means create contact id is null
           FloatingActionButton(
-              onPressed: () => showSheet(null),
-              child: Icon(Icons.add)),
+              onPressed: () => showSheet(null), child: const Icon(Icons.add)),
+      body: isloading
+          ? const Center(
+              child: Text("NO Data"),
+            )
+          : ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(contacts[index]["name"]),
+                    subtitle: Text(contacts[index]['phone']),
+                    trailing: Wrap(
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              showSheet(contacts[index]['id']);
+                            },
+                            icon: Icon(Icons.edit)),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                      ],
+                    ),
+                  ),
+                );
+              }),
     );
   }
 
   void showSheet(int? id) {
-    if (id != null) {}
+    if (id != null) {
+      //element['id']  = id from database
+      //firstWhere - first element from contacts that satisfy this condition - element['id'] == id
+      final data_from_that_id = contacts.firstWhere((element) => element['id'] == id );
+      name_controller.text = data_from_that_id['name'];
+      phone_controller.text= data_from_that_id['phone'];
+    }
     showModalBottomSheet(
         elevation: 5,
         isScrollControlled: true,
@@ -74,10 +111,12 @@ class _SqfliteCrudState extends State<SqfliteCrud> {
                       if (id != null) {
                         //await updateContact();
                       }
+                      Navigator.of(context).pop();
+                      name_controller.text = "";
+                      phone_controller.text = "";
                     },
                     child:
-                        Text(id == null ? "Create Contact" :
-                        "Update Contact"))
+                        Text(id == null ? "Create Contact" : "Update Contact"))
               ],
             ),
           );
@@ -85,11 +124,7 @@ class _SqfliteCrudState extends State<SqfliteCrud> {
   }
 
   Future<void> createContact() async {
-    await SQLHelper.create_contact(
-        name_controller.text,
-        phone_controller.text);
+    await SQLHelper.create_contact(name_controller.text, phone_controller.text);
     loadData(); // to refresh ui whenever we add a new contact
   }
-
-
 }
