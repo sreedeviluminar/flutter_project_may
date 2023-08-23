04995c2fac9ed9d6e2b167d2cbdf45dace612a18
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-23 12:50:38 +0530

    commit

--- lib/storage/firebase_storegae_example/storedata to firebase.dart
+++ lib/storage/firebase_storegae_example/storedata to firebase.dart
@@ -1,10 +1,15 @@
 import 'dart:io';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(home: FireStorageeee(),));
+}
 class FireStorageeee extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => FireStorageeeeState();
