3f05d12e0e9865bbdb7ce52c37db39ca558c861a
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-24 12:37:39 +0530

    commit

--- lib/storage/firebase_storegae_example/storedata to firebase.dart
+++ lib/storage/firebase_storegae_example/storedata to firebase.dart
@@ -1,15 +1,19 @@
 import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(home: FireStorageeee(),));
+  runApp(MaterialApp(
+    home: FireStorageeee(),
+  ));
 }
+
 class FireStorageeee extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => FireStorageeeeState();
@@ -49,7 +53,20 @@ class FireStorageeeeState extends State {
                         AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
                       if (snapshot.connectionState == ConnectionState.done) {
                         return ListView.builder(
-                            itemBuilder: (context, index) {});
+                          itemCount:snapshot.data?.length ?? 0 ,
+                            itemBuilder: (context, index) {
+                          final Map<String, dynamic> image = snapshot.data![index];
+                          return Card(
+                              child: ListTile(
+                            leading: Image.network(image['url']),
+                            title: Text(image['uploaded_by']),
+                            subtitle: Text(image['description']),
+                            trailing: IconButton(
+                                onPressed: () {
+                                  deleteImage(image['path']);
+                                }, icon: const Icon(Icons.delete)),
+                          ));
+                        });
                       }
                       return const Center(child: CircularProgressIndicator());
                     }))
@@ -59,7 +76,25 @@ class FireStorageeeeState extends State {
     );
   }
 
-  loadData() {}
+  Future<List<Map<String, dynamic>>> loadData() async {
+    List<Map<String, dynamic>> images = [];
+    final ListResult result = await storage.ref().list();
+    final List<Reference> allfiles = result.items;
+
+    await Future.forEach(allfiles, (singleFile) async {
+      final String fileurl = await singleFile.getDownloadURL();
+      final FullMetadata filemeta = await singleFile.getMetadata();
+
+      images.add({
+        'url': fileurl,
+        'path': singleFile.fullPath,
+        'uploaded_by': filemeta.customMetadata?['uploaded_by'] ?? 'No Data',
+        'description':
+            filemeta.customMetadata?['description'] ?? 'No Description'
+      });
+    });
+    return images;
+  }
 
   Future<void> upload(String imginput) async {
     final picker = ImagePicker();
@@ -86,5 +121,10 @@ class FireStorageeeeState extends State {
     } catch (e) {
       print(e);
     }
+  }
+
+  Future<void> deleteImage(String imagepath) async{
+    await storage.ref(imagepath).delete();
+    setState(() {});
   }
 }
